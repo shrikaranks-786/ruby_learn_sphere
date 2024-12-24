22fd15198cba8eb2a6df381a9a78b366fffa1f25
shrikaran ks <shrikaranksise2024@gmail.com>
2024-12-24 19:21:01 +0530

    made a landing page for the website and changed the routes.rb

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :landing ] # Landing action doesn't require authentication
+
+  def landing
+    if user_signed_in?
+      redirect_to lobby_path # Redirect authenticated users to posts#index
+    end
+  end
+end
