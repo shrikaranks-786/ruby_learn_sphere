a08f642e151228fc50768f0c20f787530613cbc7
shrikaran ks <shrikaranksise2024@gmail.com>
2024-12-26 16:15:04 +0530

    completed with the styling of the login and signup pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_24_064550) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_26_104325) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -66,6 +66,16 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_24_064550) do
     t.index ["post_id"], name: "index_categories_posts_on_post_id"
   end
 
+  create_table "lesson_users", force: :cascade do |t|
+    t.integer "lesson_id", null: false
+    t.integer "user_id", null: false
+    t.boolean "completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["lesson_id"], name: "index_lesson_users_on_lesson_id"
+    t.index ["user_id"], name: "index_lesson_users_on_user_id"
+  end
+
   create_table "lessons", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -105,5 +115,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_24_064550) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "lesson_users", "lessons"
+  add_foreign_key "lesson_users", "users"
   add_foreign_key "lessons", "posts"
 end
