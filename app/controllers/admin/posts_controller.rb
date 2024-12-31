ed9a5e1b44ae9afa7252bf192103dc497333ab13
shrikaran ks <shrikaranksise2024@gmail.com>
2024-12-31 12:13:11 +0530

    .

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,4 +2,27 @@ class Admin::PostsController < AdminController
   def index
     @admin_posts = Post.all
   end
+
+  def new
+    @admin_post = Post.new
+  end
+
+  def create
+    @admin_post = Post.new(post_params)
+
+    if @admin_post.save
+      redirect_to admin_post_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @admin_post
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title,:description,:paid,:image)
+  end
 end
