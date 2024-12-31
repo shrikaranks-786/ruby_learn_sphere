11f2dbfed6afac0cb575862c666e313acfa8ab5f
shrikaran ks <shrikaranksise2024@gmail.com>
2024-12-31 19:44:24 +0530

    added the _lesson.html.erb with some frontend changes and also render the lesson and images

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,7 @@
 class Lesson < ApplicationRecord
-  has_one_attached :video
+  has_one_attached :video do |attachable|
+    attachable.variant :thumb, resize_to_limit: [ 500, 500 ]
+  end
   belongs_to :post
   has_many :lessons_users, dependent: :destroy
 end
