4c46b6f907153e00242650a4ac6a7096da92e0c2
shrikaran ks <shrikaranksise2024@gmail.com>
2025-01-06 13:34:40 +0530

    removed the font-awsome js import tag and used the gem instead

--- Gemfile
+++ Gemfile
@@ -64,7 +64,7 @@ group :test do
   gem "selenium-webdriver"
 end
 
-gem "font-awesome-sass", "~> 6.5.1"
+gem "font-awesome-sass"
 
 gem "devise", "~> 4.9"
 
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,7 @@
-# Pin npm packages by running ./bin/importmap
-
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@fortawesome/fontawesome-free", to: "@fortawesome--fontawesome-free.js" # @6.7.2
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
